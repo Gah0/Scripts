bdf82ca3fc882c84ba5d05232c1ed12bb789972b
Gah0 <woshishacha3@gmail.com>
2019-08-26 22:06:37 +0800

    C: Calculate square area
    Signed-off-by: Gah0 <woshishacha3@gmail.com>

---
+++ C/square.c
@@ -0,0 +1,11 @@
+#include "stdio.h"
+#include "math.h"
+int main()
+{
+	int length,y=2;
+
+ printf("Press a number of length:");
+	scanf("%d",&length);
+	printf("answer:%f\n",pow(length,y));
+	return 0;
+}
