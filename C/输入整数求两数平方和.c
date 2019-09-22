b3620204db78b804e21e47d2f9ea8c327375074d
Gah0 <woshishacha3@gmail.com>
2019-09-22 20:31:50 +0800

    作业: C: 输入数值计算两数平方和！

---
+++ C/输入整数求两数平方和.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+#include "math.h"
+
+int main() {
+	int answer,value1,value2,i,y=2;
+	printf("Press two numbers to calculate sum of squares(Separated by spaces)");
+	scanf("%d %d",&value1,&value2);
+	while(i<100)
+	{
+	if (value1-(int)value1>=0&&value2-(int)value2>=0){
+		printf("Please enter integer");
+		i += 1;
+		continue;
+		}
+	else
+		answer = value1 + value2;
+		printf("the answer is %f",pow(answer,y));
+		i += 1;
+		break;
+	}
+	return 0;
+}
