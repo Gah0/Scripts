e74db2e5cd5dc56c99cb51b62e73a8dd1617d64e
Gah0 <woshishacha3@gmail.com>
2019-09-22 21:13:25 +0800

    作业: C: 利用数值找最小数！

---
+++ C/利用数组找最小数.c
@@ -0,0 +1,21 @@
+#include "stdio.h"
+
+int main()
+{	
+	int i,num[4];
+	float min=0.0;
+	printf("分别输入4个整数，输出最小值:\n");
+    for(i=0;i<4;i++)
+    {
+     scanf("%d",&num[i]);
+     break;
+    }
+    min = num[0];
+	for(i=1;i<4;i++)
+	{
+		if(num[i]<min)
+			min=num[i];
+	}
+	printf("%f\n", min);
+    return 0;
+}
