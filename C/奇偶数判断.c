60f579334e7112f57dff62363a1b6e390f4d04fd
Poilly <woshishacha3@gmail.com>
2019-10-21 11:13:50 +0800

    作业: C: 修复奇偶数判断编译不通过问题
    应该这样，把数组num放到指针变量P，这样应该可以计算？
    还有4个warning，我不想修复，加入-w和-std c99可以防止报错

--- C/奇偶数判断.c
+++ C/奇偶数判断.c
@@ -1,23 +1,24 @@
 #include "stdio.h"
 #include "conio.h"
-#include "iostream.h"
 
 int main (){
-    int num [];
-    int even,odd,num;
+    int i,n,even,odd;
     char c;
+    char num [n+1];//定义可变长数组
+    char p = &num[n+1];
 
     printf("请输入你想输入的数字，按Y结束输入并计算和\n");
     scanf("%d",&num);
 
     for(i=0; i<num; i++){
-    if(num%2==0)
-        even += a[i]
-        print("你输入的值%d已放入数组，按Y求奇数和/偶数和\n",even);
-    else
-        odd += a[i]
-        print("你输入的值%d已放入数组，按Y求奇数和/偶数和\n",odd);
-
+        if(p%2==0){
+            even += num[i];
+            printf("你输入的值%d已放入数组，按Y求奇数和/偶数和\n",even);
+        }
+        else{
+            odd += num[i];
+            printf("你输入的值%d已放入数组，按Y求奇数和/偶数和\n",odd);
+        }
     if(getch =='^Z')
         break;
     }
