4049f756245cca7ca2107cda2e4e739e50d2b8cd
Poilly <woshishacha3@gmail.com>
2020-02-21 04:06:15 +0800

    Python: 跑马灯

---
+++ Python/跑马灯.py
@@ -0,0 +1,16 @@
+import os
+import time
+
+def main():
+    content = '天皇孙笑川？......'
+
+    while True:
+        os.system('cls')
+        print(content)
+        time.sleep(0.2)
+        content = content[1:]+content[0]
+
+
+if __name__ == '__main__':
+
+    main()
