5f696787be5d857cffee8e8f9ba7f587a578a79b
Poilly <woshishacha3@gmail.com>
2020-02-25 22:02:39 +0800

    Python: 类与对象2

---
+++ Python/类与对象2.py
@@ -0,0 +1,12 @@
+def main():
+    stu1 = Student('朱嘉浩',56)
+    stu1.study('Python程序设计')
+    stu1.watct_movie()
+
+    stu2 = Student('王大锤', 15)
+    stu2.study('思想品德')
+    stu2.watch_movie()
+
+
+if __name__ == '__main__':
+    main()
