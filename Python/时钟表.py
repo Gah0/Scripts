d13a179dcf9258efbb78084a43f860f46ccfb85e
Poilly <woshishacha3@gmail.com>
2020-03-01 01:04:51 +0800

    Python: 时钟表的改进2

--- Python/时钟表.py
+++ Python/时钟表.py
@@ -2,10 +2,10 @@ from time import time,localtime,sleep
 
 class Clock(object):
 
-    def __init__(self,hour=0,minute=0,second=0):
-        self._hour=hour
-        self._minute=minute
-        self._second=second
+    def __init__(self,hour = 0, minute = 0, second = 0):
+        self._hour = hour
+        self._minute = minute
+        self._second = second
 
     @classmethod
     def now(cls):
@@ -13,15 +13,15 @@ class Clock(object):
         return cls(ctime.tm_hour, ctime.tm_min, ctime.tm_sec)
 
     def run(self):
-        self._second+=1
-        if self._second ==60:
-            self._second =0
-            self._minute +=1
-            if self._minute ==60:
-                self._minute=0
+        self._second += 1
+        if self._second == 60:
+            self._second = 0
+            self._minute += 1
+            if self._minute == 60:
+                self._minute = 0
                 self._hour+=1
-                if self._hour +=1
-                self._hour =0
+                if self._hour == 24:
+                self._hour = 0
 
     def show(self):
         return  '%02d:%02d:%02d' % \
@@ -34,5 +34,5 @@ def main():
         sleep(1)
         clock.run()
 
-if __name__ == 'main':
+if __name__ == '__main__':
     main()
