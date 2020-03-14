43bc561fb70bd9d43b01ef131e59aa65fc5931f2
Poilly <woshishacha3@gmail.com>
2020-03-15 01:59:19 +0800

    C: 线性表
    复习

--- C/线性表.c
+++ C/线性表.c
@@ -1,90 +1,85 @@
-//线性表
-
 #include "stdio.h"
+#include <iostream.h>
+using namespace std;
 
-#define LIST_INIT_SIZE 10
-#define LISTINCREMENT 2
+#define Maxsize 10
+
+Typedef int ElemType;
 
 typedef struct{
-    elemtype *elem;
-    int lenth;//4个字节
-    int listsize;
-}sqlist
+    ElemType *elem;
+    int length; //表的长度。
+}SqList;
 
-int initlist(sqlist *L)
+ElemType init_list(SqList &L)
 {
-    //创建一个空的线性数据表
-    L->elem=(elemtype* )malloc(LIST_INIT_SIZE* sizeof(elemtype));
-    if(!L->elem)
-        printf("overflow");
-        exit 0;
-
-    L->lenth=0;
-    L->listsize=LIST_INIT_SIZE;
-    printf("sucess!")
-}
-
-
-int LocateElem(sqlist L,elemtype e)
-{
-    elemtype *p;
-    int i = 1;
-
-    p = L.elem;
-
-    while(i<L.lenth&&(*p++)!=e)
-        i++;
-    
-    if(i<=L.lenth)
-        return i;
-    else
+    L.elem = new ElemType [Maxsize];
+    if(!L.elem)
+        printf("Overflow on elem");
         return 0;
+    L.length = 0;
+    return 1;
 }
 
-int listinsert(sqllist *L,int i,elemtype e)
+ElemType list_create(SqList &L)
 {
-    elemtype *newbase,*q,*p;
-    int j;
-
-    if(i<1||i>L->lenth+1)
-        printf("error")
-
-    if(L->lenth>=L->listsize)
+    int x,i=0;
+    while(x>Maxsize)
     {
-        //当前储存空间已满，增加分配
-        newbase=(elemtype* )realloc((*L).elem)
-            ((*L).listsize+LISTINCREMENT)*sizeof(elemtype))
-        
-        if(!newbase)
-            printf("空间已满\n");
-        
-        L->elem=newbase;
-        L->listsize=L->listsize+LISTINCREMENT;
-
+        if(L.length == Maxsize)
+        {
+            cout<<"顺序表满了"
+            return 0;
+        }
+    cin>>x;
+    L.elem[i++];
+    L.length++;
     }
-
-    for(j=(L->lenth)-1; j>=i-1; j--)
-        L->elem[j+1]=L->elem[j];
-    
-    L->elem[i-1]=e
-    ++L->lenth;
-    printf("insert OK");
-    
+    return 1;
 }
 
-int lisadelete(sqlist *L,int i, elemtype e)
+ElemType get_elem(SqList &L, int i, int &e)
 {
-    elemtype *p, *q;
-    int j;
-
-    if(i<1||i>L>lenth)
-        printf("error")
+    if(i<1||i>L.length)
+        return false;
     
-    e=L->elem[i-1];
+    e=L.elem[i-1];
+    return 1;
+}
 
-    for(j=i; j<L->lenth; j++)
-        L->elem[j-1]=L->elem[j];
-        L->lenth++;
-        return e
+ElemType locate_elem(SqList &L, int e)
+{
+    for(i=0;i<L.length;i++)
+        if(L.elem[i] == e)
+            return i+1;
+    return -1;
+}
+
+ElemType insert_elem(SqList &L, int i, int e)
+{
+    int j=0;
+
+    if(i<1||i>L.length+1)
+        return false;
+
+    if(L.length == Maxsize)
+        return false;    
+
+    for(int j=i;j<L.length;j++)
+        L.elem[i+1]=L.elem[j]
+        L.length++;
+    
+    return 1;
+}
+
+ElemType delete_elem(SqList &L, int i, int e)
+{
+    if(i<1 || i>L.Length)
+        return false;
+
+    e = L.elem[i-1];//把index为i的元素赋值给e   0123i5678
+    for(int j=i;j<=L.length-1;j++)
+        L.elem[j-1]=L.elem[j];//把index为5的j元素赋值给j的上一个位置
+        L.length--;
 }
 
