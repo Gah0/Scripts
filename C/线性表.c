0ed67047bc93a7e91f936e08822649e862b0b196
Poilly <woshishacha3@gmail.com>
2020-03-26 02:16:49 +0800

    C: 线性表.c: add testfunctions() on main

--- C/线性表.c
+++ C/线性表.c
@@ -1,67 +1,109 @@
 #include <stdio.h>
-#include <ctype>
+#include <ctype.h>
+#include <stdlib.h>
 
-#define Maxsize 10;
+#define Maxsize 10
 
+int creatlist(struct sqlist *sql);
+int locateelem(struct sqlist *sql,int elem);
+int listinsert(struct sqlist *sql, int i, int e);
+int listdelete(struct sqlist *sql,int i,int e);
+void displaysql(struct sqlist *sql);
 
 typedef struct sqlist{
     int *elem;
     int length;
 };
 
-
-void main (){
-    int ret,input;
+int main (){
+    int i,input,nums,sq;
+    int err=0;
     struct sqlist sql;
-    printf("请输入一个数字：");
+	printf("请输入小键盘数字操作\n");
+	printf("(1)添加数字到链条\n(2)从链条删除数字\n(3)查找数字\n(4)输出sqlist\n");
     scanf("%d",&input);
 
-    if (isdigit(input)){
-        sql.elem = int [Maxsize];
-    }
-    else {
-        printf("请输入一个有效的数字！");
-    }
+    do{
+        switch(input){
+            case 1:
+                    printf("请输入你要添加的位置和数字:");
+                    scanf("%d,%d",&i,&e);
+                    creatlist(&sql);
+                    sq = listinsert(&sql,i,e);
+                    if(sq==err){
+                        printf("删除不成功");
+                    }
+                    else{
+                        printf("成功添加%d",sq);  
+                    }
+                }
+                else {
+                    printf("请输入一个有效的数字添加到链条！");
+                }
+
+            case 2:
+                printf("请输入你要删除的数字");
+                scanf("%d,%d",&i,&e);
+                sq=listdelete(&sql,i,e);
+                if(sq==err){
+                    printf("删除不成功");
+                }else{
+                    printf("成功删除%d",sq); 
+                } 
+
+            case 3:
+                printf("请输入你要查找的数字");
+                scanf("%d",&e);
+                sq=locateelem(&sql,e);
+                printf("你的数字在第%d个\n",sq.i);
+
+            case 4:
+                displaysql();
+
+            default:
+				printf("没有此功能！\n");
+				break;
+        }while(1);
+    return 0;
 }
 
-void creatlist(struct sqlist *sql){
+int creatlist(struct sqlist *sql){
     int i;
 
     sql->elem = malloc(Maxsize * sizeof(int));
     if(!sql->elem){
         printf("内存分配失败");
-        return ERROR;
+        return main();
     }
     sql->length=0;
-    return OK;
 }
 
-void locateelem(struct sqlist *sql,int elem){
+int locateelem(struct sqlist *sql,int elem){
     int *p;
     int i=1;
-    p=sql.elem;
+    p=sql->elem;
     while(i<sql.length&&(*p++)!=elem)//时间复杂度为O(n)  平均比较次数为n+1/2
     {
         ++i;
     }
-    if(i<=sql.length)
+    if(i<=sql->length)
         return i;
     else
         return 0;
 }
 
-void listinsert(struct sqlist *sql, int i, int e){//顺序表存在，当1<i<length+1时，在第i个位置之前插入新数据e，然后length+1
+int listinsert(struct sqlist *sql, int i, int e){//顺序表存在，当1<i<length+1时，在第i个位置之前插入新数据e，然后length+1
     int *newsql;
     int j;
 
     if(i<1||i>sql->length+1)
-        return ERROR;
+        return err;
     if(sql->length == Maxsize){//如果储存空间满了
         newsql=(int *)realloc((*sql).elem, 
             (Maxsize + LISTINCREMENT) * sizeof(int); //堆小了，多申请2增量sizeof(int)
         if(!newsql)
             printk("连续空间已满");
-            return ERROR;
+            return err;
         sql->elem = newsql;
         sql->length = sql->Maxsize + LISTINCREMENT;
     }
@@ -72,15 +114,15 @@ void listinsert(struct sqlist *sql, int i, int e){//顺序表存在，当1<i<len
     sql->elem[i-1]=e;//在i位置插入元素e
     //表长+1
     ++sql->length;
-    return OK;
+    return e;
 }//在长度为n的线性表中插入一个元素所需移动数据元素的平均次数是  E= 求和（n+1）i=1 =n/2;
 //平均时间复杂度为O（n）
 
-void listdelete(struct sqlist *sql,int i,int e){
+int listdelete(struct sqlist *sql,int i,int e){
     int j;
     if(i<1||i>sql->length)
     {
-        return ERROR;
+        return err;
     }
     //把删除元素存入e中
     e = sql->elem[i-1];
@@ -90,4 +132,11 @@ void listdelete(struct sqlist *sql,int i,int e){
     }
     sql->length++;
     return e;
+}
+
+int displaysql(struct sqlist *sql){
+    for (int i=0;i<sql.length;i++) {
+        printf("%d ",sql.elem[i]);
+    }
+    printf("\n");
 }
