878f202792098dfeb6da39f704e114d1b4054fde
Poilly <woshishacha3@gmail.com>
2020-03-26 04:26:12 +0800

    C: 线性表.c: fix up 2

--- C/线性表.c
+++ C/线性表.c
@@ -14,49 +14,56 @@ int main (){
     int i,e,input,nums,sq;
     int err=0;
     struct sqlist sql;
-	printf("请输入小键盘数字操作\n");
-	printf("(1)添加数字到链条\n(2)从链条删除数字\n(3)查找数字\n(4)输出sqlist\n");
-    scanf("%d",&input);
 
     do{
+	printf("请输入小键盘数字操作\n");
+	printf("(1)添加数字到链条\n(2)从链条删除数字\n(3)查找数字\n(4)输出sqlist\n\0");
+    scanf("%d",&input);
+
         switch(input){
             case 1:
-                    printf("请输入你要添加的位置和数字:");
+                    printf("请输入你要添加的位置和数字:\0");
                     scanf("%d,%d",&i,&e);
-                    if (isdigit(input)){
-                    creatlist(&sql);
-                    sq = listinsert(&sql,i,e);
+                    if (isdigit(e) && isdigit(i)){
+                        creatlist(&sql);
+                        sq = listinsert(&sql,i,e);
                         if(sq == err){
-                            printf("删除不成功");
+                            printf("删除不成功\0");
                         }
                         else{
-                            printf("成功添加%d",sq);
+                            printf("成功添加%d\0",sq);
                         }
                     }
                     else {
-                        printf("请输入一个有效的数字添加到链条！");
+                        printf("请输入一个有效的数字添加到链条！\0");
                     }
+                break;
+
             case 2:
-                printf("请输入你要删除的数字");
+                printf("请输入你要删除的数字\0");
                 scanf("%d,%d",&i,&e);
                 sq = listdelete(&sql,i,e);
                 if(sq == err){
-                    printf("删除不成功");
+                    printf("删除不成功\0");
                 }else{
-                    printf("成功删除%d",sq);
+                    printf("成功删除%d\0",sq);
                 }
+                break;
 
             case 3:
-                printf("请输入你要查找的数字");
+                printf("请输入你要查找的数字\0");
                 scanf("%d",&e);
                 sq = locateelem(&sql,e);
-                printf("你的数字在第%d个,错误会返回0\n",sq);
+                printf("你的数字在第%d个,错误会返回0\n\0",sq);
+                break;
 
             case 4:
                 sq = displaysql(&sql);
                 printf("%4d",sq);
+                break;
+
             default:
-				printf("没有此功能！\n");
+				printf("没有此功能！\n\0");
 				break;
         }
     }while(1);
@@ -68,7 +75,7 @@ int creatlist(struct sqlist *sql){
 
     sql->elem = malloc(Maxsize * sizeof(int));
     if(!sql->elem){
-        printf("内存分配失败");
+        printf("内存分配失败\0");
         return main();
     }
     sql->length=0;
@@ -104,7 +111,7 @@ int listinsert(struct sqlist *sql, int i, int e){//顺序表存在，当1<i<leng
     }
 
     if(!newsql){
-        printf("连续空间已满");
+        printf("连续空间已满\0\n");
         return err;
     }
     sql->elem = newsql;
