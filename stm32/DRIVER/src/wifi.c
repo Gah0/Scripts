c5b7caa858a7ae361d8c7983eb30cfe445822f91
Poilly <g0d3ph@gmail.com>
2022-03-31 16:41:46 +0800

    stm32:  Add delay time to wait for restart

--- stm32/DRIVER/src/wifi.c
+++ stm32/DRIVER/src/wifi.c
@@ -3,8 +3,10 @@
 
 
 void WIFI_Rst(void){
-	USART_SendString(USART1,"RST module...\r\n");
+  USART_SendString(USART1,"RST module...\r\n");
   USART_SendString(USART3,"AT+RST\r\n");
+  Delay_us(3000);
+  Delay_us(3000);
 }
 
 void WIFI_Init(void){		
